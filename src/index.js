d99a2dd02e55290d40020dc74015ca8ec9db8590
TheMaxON <sldpsnow8800@gmail.com>
2022-12-05 19:57:09 +0200

    base WO/ icon

---
+++ src/index.js
@@ -0,0 +1,15 @@
+// MODAL RESERVE
+(() => {
+    const refs = {
+      openModalTableBtn: document.querySelector("[data-modal-table-open]"),
+      closeModalTableBtn: document.querySelector("[data-modal-table-close]"),
+      modalTable: document.querySelector("[data-modal-table]"),
+    };
+  
+    refs.openModalTableBtn.addEventListener("click", toggleModalTable);
+    refs.closeModalTableBtn.addEventListener("click", toggleModalTable);
+  
+    function toggleModalTable() {
+        refs.modalTable.classList.toggle("visually-hidden");
+    }
+  })();
