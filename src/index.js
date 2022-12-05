ce4d4a5d0045d893566d5d10721f79d1d9ae110c
TheMaxON <sldpsnow8800@gmail.com>
2022-12-06 00:43:18 +0200

    Fixes

--- src/index.js
+++ src/index.js
@@ -13,3 +13,18 @@
         refs.modalTable.classList.toggle("visually-hidden");
     }
   })();
+
+  // MODAL RESERVE 2
+(() => {
+  const refs = {
+    openModalTableBtn: document.querySelector("[data-modal-table-open__restaurant-mainpage]"),
+    modalTable: document.querySelector("[data-modal-table]"),
+  };
+
+  refs.openModalTableBtn.addEventListener("click", toggleModalTable);
+  refs.closeModalTableBtn.addEventListener("click", toggleModalTable);
+
+  function toggleModalTable() {
+      refs.modalTable.classList.toggle("visually-hidden");
+  }
+})();
