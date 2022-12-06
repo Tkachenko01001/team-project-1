80bd3f8e695f5027a575a144a1d45acdd49ecb7c
TheMaxON <sldpsnow8800@gmail.com>
2022-12-06 03:44:14 +0200

    almost done

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-// MODAL RESERVE
+// MODAL TABLE RESERVE
 (() => {
     const refs = {
       openModalTableBtn: document.querySelector("[data-modal-table-open]"),
@@ -14,17 +14,34 @@
     }
   })();
 
-  // MODAL RESERVE 2
+  // MODAL TABLE RESERVE 2
+// (() => {
+//   const refs = {
+//     openModalTableBtn: document.querySelector("[data-modal-table-open__restaurant-mainpage]"),
+//     modalTable: document.querySelector("[data-modal-table]"),
+//   };
+
+//   refs.openModalTableBtn.addEventListener("click", toggleModalTable);
+//   refs.closeModalTableBtn.addEventListener("click", toggleModalTable);
+
+//   function toggleModalTable() {
+//       refs.modalTable.classList.toggle("visually-hidden");
+//   }
+// })();
+
+// MODAL ROOM RESERVE
 (() => {
   const refs = {
-    openModalTableBtn: document.querySelector("[data-modal-table-open__restaurant-mainpage]"),
-    modalTable: document.querySelector("[data-modal-table]"),
+    openModalRoomBtn: document.querySelector("[data-modal-room-open]"),
+    closeModalRoomBtn: document.querySelector("[data-modal-room-close]"),
+    modalRoom: document.querySelector("[data-modal-room]"),
   };
 
-  refs.openModalTableBtn.addEventListener("click", toggleModalTable);
-  refs.closeModalTableBtn.addEventListener("click", toggleModalTable);
+  refs.openModalRoomBtn.addEventListener("click", toggleModalRoom);
+  refs.closeModalRoomBtn.addEventListener("click", toggleModalRoom);
 
-  function toggleModalTable() {
-      refs.modalTable.classList.toggle("visually-hidden");
+  function toggleModalRoom() {
+      refs.modalRoom.classList.toggle("visually-hidden");
+      console.log('modal opening')
   }
 })();
