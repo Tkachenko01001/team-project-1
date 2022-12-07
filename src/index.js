f06fc7f6e19563c92505e6cdb87bdd617328ed6c
yanyanovich <yan.tokaruk@gmail.com>
2022-12-07 10:17:05 +0200

    slider

--- src/index.js
+++ src/index.js
@@ -1,30 +1,32 @@
 // MODAL RESERVE
 (() => {
-    const refs = {
-      openModalTableBtn: document.querySelector("[data-modal-table-open]"),
-      closeModalTableBtn: document.querySelector("[data-modal-table-close]"),
-      modalTable: document.querySelector("[data-modal-table]"),
-    };
-  
-    refs.openModalTableBtn.addEventListener("click", toggleModalTable);
-    refs.closeModalTableBtn.addEventListener("click", toggleModalTable);
-  
-    function toggleModalTable() {
-        refs.modalTable.classList.toggle("visually-hidden");
-    }
-  })();
-
-  // MODAL RESERVE 2
-(() => {
   const refs = {
-    openModalTableBtn: document.querySelector("[data-modal-table-open__restaurant-mainpage]"),
-    modalTable: document.querySelector("[data-modal-table]"),
+    openModalTableBtn: document.querySelector('[data-modal-table-open]'),
+    closeModalTableBtn: document.querySelector('[data-modal-table-close]'),
+    modalTable: document.querySelector('[data-modal-table]'),
   };
 
-  refs.openModalTableBtn.addEventListener("click", toggleModalTable);
-  refs.closeModalTableBtn.addEventListener("click", toggleModalTable);
+  refs.openModalTableBtn.addEventListener('click', toggleModalTable);
+  refs.closeModalTableBtn.addEventListener('click', toggleModalTable);
 
   function toggleModalTable() {
-      refs.modalTable.classList.toggle("visually-hidden");
+    refs.modalTable.classList.toggle('visually-hidden');
+  }
+})();
+
+// MODAL RESERVE 2
+(() => {
+  const refs = {
+    openModalTableBtn: document.querySelector(
+      '[data-modal-table-open__restaurant-mainpage]'
+    ),
+    modalTable: document.querySelector('[data-modal-table]'),
+  };
+
+  refs.openModalTableBtn.addEventListener('click', toggleModalTable);
+  refs.closeModalTableBtn.addEventListener('click', toggleModalTable);
+
+  function toggleModalTable() {
+    refs.modalTable.classList.toggle('visually-hidden');
   }
 })();
